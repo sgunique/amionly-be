e25ff3c9e9eb599a6a63671e3d2f4dc3f05caa96
Santosh Gudikandula <sgudikandula@smavas-MBP-7.fritz.box>
2019-11-28 22:54:42 +0100

    springboot base project setup

---
+++ src/main/java/com/amionly/survey/AmIonlySurveyApplication.java
@@ -0,0 +1,13 @@
+package com.amionly.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmIonlySurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmIonlySurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amionly/survey/AmIonlySurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amionly.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmIonlySurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
